317efacef004d872b9e921e8ea0001258c18ccb4
Connor <bconnor@bell.corp.bce.ca>
2019-06-25 10:30:29 -0400

    added weather and calendar

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-Hello World!
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        </head>
+        <body>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" tpye="text/javascript"></script>
+		<!--<script src="http://code.jquery.com/jquery-1.8.3.js" tpye="text/javascript"></script>-->
+		<!--<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js" tpye="text/javascript"></script>-->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" tpye="text/javascript"></script>
+		<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.js"></script>
+
+<div><a class="weatherwidget-io" href="https://forecast7.com/en/43d90n78d94/whitby/" data-label_1="WHITBY" data-label_2="WEATHER" data-theme="original" >WHITBY WEATHER</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script></div>
+<div><iframe src="https://calendar.google.com/calendar/embed?src=katie.szypniewski%40gmail.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe></div>
+<iframe src="https://open.spotify.com/browse/featured" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</body>
+</html>
